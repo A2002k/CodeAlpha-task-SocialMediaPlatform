8fd63c985129a910b3c876d75e0dc6bbaa40830b
Anthony Karam <karamzanthony2002@gmail.com>
2026-07-04 01:21:10 +0300

    Fix API URL and update CORS for deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://codealpha-task-socialmediaplatform.onrender.com/api",
 });
 
+// Attach token automatically
 API.interceptors.request.use((req) => {
-  const token = sessionStorage.getItem("token");
+  const token = sessionStorage.getItem("token") || localStorage.getItem("token");
 
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
-
+   
   return req;
 });
 
